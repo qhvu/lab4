2796a0dddf5bff34dffedd9e23bab108d67acf97
QH <hqv001@ucsd.edu>
2020-01-30 11:00:07 -0800

    node.js lab

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
@@ -41,6 +42,12 @@ app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
 
+app.get('/project/:name',project.viewProject);
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
